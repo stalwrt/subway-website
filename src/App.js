d3de18ecffb8708758933027d427334758a2a87e
_floreesleo <retr0.strwll@gmail.com>
2023-03-04 12:49:55 -0600

    Creacion de helper para peticiones AJAX - Video 2 REACT

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import CrudApi from './components/CrudApi';
 import CrudApp from './components/CrudApp';
 
 function App() {
 	return (
 		<>
 			<h1>Ejercicio con React</h1>
+			<hr />
+			<CrudApi />
+			<hr />
 			<CrudApp />
 		</>
 	);
